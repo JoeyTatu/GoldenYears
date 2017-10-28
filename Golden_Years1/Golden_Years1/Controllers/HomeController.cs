8abdd0bbc90652e11bc23ea04610a6595c17c5de
Keith Feeney <Keith.Feeney@student.ncirl.ie>
2017-10-28 10:56:15 +0100

    Adding Visual Studio Project and Week 6 Peer Review

---
+++ Golden_Years1/Golden_Years1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Golden_Years1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Events page";
+
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Education page";
+
+            return View();
+        }
+
+        public ActionResult Medical()
+        {
+            ViewBag.Message = "Medical page";
+
+            return View();
+        }
+    }
+}
---
+++ Golden_Years1/Golden_Years1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Golden_Years1.MvcApplication" Language="C#" %>
